26e71f610086aec37182e18565d545500b9fcb18
Jordan Wu <jordan.spencer.wu@gmail.com>
2015-12-05 13:47:28 -0700

    add chapter 7

---
+++ SportsStore.Domain/Conrete/EFProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.Domain.Conrete
+{
+    public class EFProductRepository : IProductsRepository
+    {
+        private EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}
